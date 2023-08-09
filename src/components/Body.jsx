42dcdb37fce72c1268086494d8a2939940b93b5d
Daksh Prajapati <89261276+Daksh-Prajapati-49@users.noreply.github.com>
2023-08-09 23:55:42 +0530

    Created Stocklist and Graph

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import StockList from './StockList'
+import Graph from './Graph'
 
 const Body = () => {
   return (
-    <div>
-        
+    <div className="flex">
+        <div className="w-1/3">
+            <StockList/>
+        </div>
+        <div className="w-2/3">
+            <Graph/>
+        </div>
+
     </div>
   )
 }
