d1e1312d66a5389668db5ea219a82414f5e9862e
Daksh Prajapati <89261276+Daksh-Prajapati-49@users.noreply.github.com>
2023-08-10 20:28:11 +0530

    Updated modals

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,36 +1,55 @@
 import React, { useState } from 'react'
+import Modal from './Modal'
 
 const Body = () => {
   const [amt, setAmt] = useState(0)
   const [window, setWindow] = useState("")
   const [date, setDate] = useState("")
 
+  // Modal 
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="flex justify-center">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex justify-center flex-col">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="amt">
             Principal Amount
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="number" value={amt} onChange={(e)=>{setAmt(e.value)}}/>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="number" value={amt} onChange={(e) => { setAmt(e.value) }} />
         </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-            Window
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="factor">
+            Factor
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" value={window} onChange={(e)=>{setWindow(e.value)}}/>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" value={window} onChange={(e) => { setWindow(e.value) }} />
         </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="date">
             Date
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="date" placeholder="Username" value={date} onChange={(e)=>{setDate(e.value)}}/>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="date" placeholder="Username" value={date} onChange={(e) => { setDate(e.value) }} />
         </div>
-        <div class="flex items-center justify-between">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        {/* <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
             Submit
           </button>
-        </div>
+        </div> */}
+
+        <button
+          className="bg-blue-800 text-white active:bg-blue-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          Open regular modal
+        </button>
+        
+        <Modal setShowModal={setShowModal} showModal={showModal}/>
+
+        
+
+
+
       </form>
     </div>
   )
