b651da11fe38c4f30d2f86fed76e2ff09725ab7c
Daksh Prajapati <89261276+Daksh-Prajapati-49@users.noreply.github.com>
2023-08-10 18:29:46 +0530

    Updated files

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import StockList from './StockList'
-import Graph from './Graph'
+import React, { useState } from 'react'
 
 const Body = () => {
-  return (
-    <div className="flex">
-        <div className="w-1/3">
-            <StockList/>
-        </div>
-        <div className="w-2/3">
-            <Graph/>
-        </div>
+  const [amt, setAmt] = useState(0)
+  const [window, setWindow] = useState("")
+  const [date, setDate] = useState("")
 
+  return (
+    <div className="flex justify-center">
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Principal Amount
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="number" value={amt} onChange={(e)=>{setAmt(e.value)}}/>
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Window
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" value={window} onChange={(e)=>{setWindow(e.value)}}/>
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Date
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="date" placeholder="Username" value={date} onChange={(e)=>{setDate(e.value)}}/>
+        </div>
+        <div class="flex items-center justify-between">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
